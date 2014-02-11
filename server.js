4a823f47cf98dfcc556faffa316f72bea36808a3
RK-WJW <javawjw@163.com>
2014-02-11 18:57:49 +0800

    server

---
+++ server.js
@@ -0,0 +1,36 @@
+var fs = require("fs");
+var ss = require("./server/ss.js");
+var hosts = require("./server/hosts.js");
+var route = require("./server/route.js");
+
+function deal(data){
+    var routes = {};
+    var server = data.server;
+    for(var i = 0; i < server.length; i++){
+        var domain = server[i].domain;
+        var port = ss.start(server[i]);//¿ªÆôserver£¬·µ»Ø¶Ë¿ÚºÅ
+        routes[domain] = port;
+        hosts.set(domain);
+    }
+    route.start(routes);
+}
+
+function start(){
+    var path = "./config.json";
+    fs.exists(path, function (t){
+        if(t){
+            fs.readFile(path, function (err, data){
+                if(err){
+                    throw err;
+                }
+                eval('var obj = ' + data);
+                deal(obj);
+            })
+        }else{
+            console.warn("file not found. " + path);
+            // callback(null);
+        }
+    });
+}
+
+start();
