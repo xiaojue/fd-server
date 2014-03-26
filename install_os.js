7965fb7e148637805e91f11adb134d7a62698084
fu <designsor@gmail.com>
2014-03-26 15:36:33 +0800

    fix sudo

--- install_os.js
+++ install_os.js
@@ -4,11 +4,11 @@ var cp = require('child_process');
 var cmd;
 
 if (platform == 'linux') {
-	cmd = cp.exec('npm install git://github.com/xiaojue/node-linux.git#5bd49b078e3342752ed14642e78922ac2cab27ba');
+	cmd = cp.exec('sudo -u root npm install git://github.com/xiaojue/node-linux.git#5bd49b078e3342752ed14642e78922ac2cab27ba');
 } else if (platform == 'win32') {
 	cmd = cp.exec('npm install node-windows');
 } else if (platform == 'darwin') {
-	cmd = cp.exec('npm install node-mac');
+	cmd = cp.exec('sudo -u root npm install node-mac');
 }
 
 cmd.stdout.on('data', function(data) {
