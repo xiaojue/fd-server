bf1006d5922ca1d5a653fb29c4ee5ee501d425e9
xiaojue <designsor@gmail.com>
2014-06-20 16:31:24 +0800

    rm *

--- test/support/simpleServer.js
+++
@@ -1,6 +0,0 @@
-var http = require("http");
-http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("Hello World!");
-    response.end();
-}).listen(8555);
